e14f1a7176d12b8597909aef37db9bc697cf1e66
kiralx <kiralx@bk.ru>
2022-09-19 23:45:06 +0300

    added lesson 2 task 13 solution third digit from the left

--- lesson_02/Program.cs
+++ lesson_02/Program.cs
@@ -14,10 +14,22 @@
 // Задача 13: Напишите программу, которая
 // выводит третью цифру заданного числа
 // или сообщает, что третьей цифры нет.
-// 645 -> 6
+// 645 -> 5
 // 78 -> третьей цифры нет
 // 32679 -> 6
 
+// ###### Это для третьей цифры слева
+
+Console.WriteLine("Введите число:");
+int n = Convert.ToInt32(Console.ReadLine()!);
+if((n < 100) && (n > -100) ) Console.WriteLine("Третьей цифры нет");
+else
+{
+    while ((n < -999) || (n > 999)) n = n / 10;
+    Console.WriteLine(n % 10);
+}
+
+// ###### Это для третьей цифры справа
 // Console.WriteLine("Введите число:");
 // int n = Convert.ToInt32(Console.ReadLine()!);
 // if((n / 100) != 0) Console.WriteLine((n / 100) % 10);
@@ -32,7 +44,7 @@
 // 7 -> да
 // 1 -> нет
 
-Console.WriteLine("Введите цифру дня недели (1-7, пнд первый):");
-int day = Convert.ToInt32(Console.ReadLine()!);
-if((day == 6) || (day == 7)) Console.WriteLine("Да, выходной!");
-else Console.WriteLine("Нет");
+// Console.WriteLine("Введите цифру дня недели (1-7, пнд первый):");
+// int day = Convert.ToInt32(Console.ReadLine()!);
+// if((day == 6) || (day == 7)) Console.WriteLine("Да, выходной!");
+// else Console.WriteLine("Нет");

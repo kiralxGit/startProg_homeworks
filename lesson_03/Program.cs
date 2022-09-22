06023ebe7c54a143faa966c0a9e18bceaf0c19ac
kiralx <kiralx@bk.ru>
2022-09-22 21:58:54 +0300

    added solution for task 21

--- lesson_03/Program.cs
+++ lesson_03/Program.cs
@@ -12,6 +12,22 @@
 // A (3,6,8); B (2,1,-7), -> 15.84
 // A (7,-5, 0); B (1,-1,9) -> 11.53
 
+Console.WriteLine("Введите x, y, z точки A через запятую: ");
+string inA = Console.ReadLine()!;
+string[] pointA = inA.Split(',');
+double[] xyzA = new double [pointA.Length];
+for (int i = 0; i < pointA.Length; i++){
+    xyzA[i] = Convert.ToDouble(pointA[i]);
+}
+Console.WriteLine("Введите x, y, z точки B через запятую: ");
+string inB = Console.ReadLine()!;
+string[] pointB = inB.Split(',');
+double[] xyzB = new double [pointB.Length];
+for (int i = 0; i < pointB.Length; i++){
+    xyzB[i] = Convert.ToDouble(pointB[i]);
+}
+double temp = Math.Sqrt(Math.Pow((xyzB[0] - xyzA[0]), 2) + Math.Pow((xyzB[1] - xyzA[1]), 2) + Math.Pow((xyzB[2] - xyzA[2]), 2));
+Console.WriteLine($"{temp:f2}");
 
 /*-------------------------------------------------------------------------*/
 // Задача 23
@@ -19,9 +35,9 @@
 // и выдаёт таблицу кубов чисел от 1 до N.
 // 3 -> 1, 8, 27
 // 5 -> 1, 8, 27, 64, 125
-Console.WriteLine("Введите N: ");
-int n = Convert.ToInt32(Console.ReadLine()!);
-for(int i = 1; i <= n; i++) {
-    Console.Write(Math.Pow(i, 3));
-    if (i < n) Console.Write(", ");
-}
+// Console.WriteLine("Введите N: ");
+// int n = Convert.ToInt32(Console.ReadLine()!);
+// for(int i = 1; i <= n; i++) {
+//     Console.Write(Math.Pow(i, 3));
+//     if (i < n) Console.Write(", ");
+// }

0e61083530164076b385626710ed42faf5665233
kiralx <kiralx@bk.ru>
2022-10-02 00:30:18 +0300

    added lesson 5 all solutions

--- lesson_05/Program.cs
+++ lesson_05/Program.cs
@@ -17,16 +17,6 @@ int GetEvenQty(int[] array){
     return count;
 }
 
-int[] GetArray(int size, int minValue, int maxValue){
-    int[] res = new int[size];
-
-    for (int i = 0; i < size; i++)
-    {
-        res[i] = new Random().Next(minValue, maxValue + 1);
-    }
-    return res;
-}
-
 Console.WriteLine();
 // Задача 36: Задайте одномерный массив, заполненный случайными числами. Найдите сумму элементов, стоящих на нечётных позициях.
 // [3, 7, 23, 12] -> 19
@@ -74,4 +64,18 @@ double GetDiff(double[] array){
         else if(array[i] < array[min]) min = i;
     }   
     return (array[max] - array[min]);
+}
+
+
+
+/* --------------------- общие методы -------------------------- */
+
+int[] GetArray(int size, int minValue, int maxValue){
+    int[] res = new int[size];
+
+    for (int i = 0; i < size; i++)
+    {
+        res[i] = new Random().Next(minValue, maxValue + 1);
+    }
+    return res;
 }

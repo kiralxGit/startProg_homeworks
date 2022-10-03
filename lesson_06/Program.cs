ba1115989444bcd1251bf318755bbc1d0c9a7cfa
kiralx <kiralx@bk.ru>
2022-10-04 00:23:47 +0300

    last

--- lesson_06/Program.cs
+++ lesson_06/Program.cs
@@ -37,6 +37,6 @@ Console.Write("Введите число k2: ");
 double k2 = Convert.ToDouble(Console.ReadLine()!);
 
 double x = (b2 - b1) / (k1 - k2);
-double y = y = k1 * x + b1;
+double y = k1 * x + b1;
 
 Console.WriteLine($"({x}; {y})");

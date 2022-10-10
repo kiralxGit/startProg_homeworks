3dd8fe0b93ab8d17bec54f997e470fe8540fdf05
kiralx <kiralx@bk.ru>
2022-10-11 00:41:12 +0300

    added lesson 8 task 60

--- lesson_08/Program.cs
+++ lesson_08/Program.cs
@@ -111,41 +111,42 @@
     // Результирующая матрица будет:
     // 18 20
     // 15 18
-Console.WriteLine("Задача 58:");
-int [,] arr581 = GetArray(m: 2, n: 2);
-//int[,] arr581 = new int[,]{{2,4}, {3,2}}; // проверка 1
-//int[,] arr581 = new int[,]{{2,4}, {3,2}}; // проверка 2
-//int[,] arr581 = new int[,]{{2,4}, {3,2}, {2,5}}; // проверка 3
+// Console.WriteLine("Задача 58:");
+// int [,] arr581 = GetArray(m: 2, n: 2);
+// //int[,] arr581 = new int[,]{{2,4}, {3,2}}; // проверка 1
+// //int[,] arr581 = new int[,]{{2,4}, {3,2}}; // проверка 2
+// //int[,] arr581 = new int[,]{{2,4}, {3,2}, {2,5}}; // проверка 3
 
-PrintArray(arr581);
-Console.WriteLine();
-int [,] arr582 = GetArray(m: 2, n: 2);
-//int[,] arr582 = new int[,]{{3,4}, {3,3}}; // проверка 1
-//int[,] arr582 = new int[,]{{3,4,1}, {3,3,4}}; // проверка 2
-//int[,] arr582 = new int[,]{{3,4,2}, {3,3,1}}; // проверка 3
+// PrintArray(arr581);
+// Console.WriteLine();
+// int [,] arr582 = GetArray(m: 2, n: 2);
+// //int[,] arr582 = new int[,]{{3,4}, {3,3}}; // проверка 1
+// //int[,] arr582 = new int[,]{{3,4,1}, {3,3,4}}; // проверка 2
+// //int[,] arr582 = new int[,]{{3,4,2}, {3,3,1}}; // проверка 3
 
-PrintArray(arr582);
-Console.WriteLine();
-if(arr581.GetLength(0) == arr582.GetLength(1)) PrintArray(MultiplyArray(arr581,arr582));
-else Console.WriteLine("Кол-во строк первой матрицы не равно кол-ву столбцов второй");
+// PrintArray(arr582);
+// Console.WriteLine();
+// if(arr581.GetLength(0) == arr582.GetLength(1)) PrintArray(MultiplyArray(arr581,arr582));
+// else Console.WriteLine("Кол-во строк первой матрицы не равно кол-ву столбцов второй");
 
-int[,] MultiplyArray(int[,] array1, int[,] array2)
-{
-    int[,] res = new int[array1.GetLength(0), array2.GetLength(1)];
-    for (int i = 0; i < array1.GetLength(0); i++)
-    {
-        for (int k = 0, sum = 0; k < array2.GetLength(1); k++)
-        {   
-            for (int r1 = 0, c2 = 0; c2 < array2.GetLength(0); r1++, c2++)
-            {
-                sum += array1[i, r1] * array2[c2, k];
-            }
-            res[i,k] = sum;
-            sum = 0;
-        }
-    }
-    return res;
-}
+// int[,] MultiplyArray(int[,] array1, int[,] array2)
+// {
+//     int[,] res = new int[array1.GetLength(0), array2.GetLength(1)];
+//     for (int i = 0; i < array1.GetLength(0); i++)
+//     {
+//         for (int k = 0, sum = 0; k < array2.GetLength(1); k++)
+//         {   
+//             for (int r1 = 0, c2 = 0; c2 < array2.GetLength(0); r1++, c2++)
+//             {
+//                 sum += array1[i, r1] * array2[c2, k];
+//             }
+//             res[i,k] = sum;
+//             sum = 0;
+//         }
+//     }
+//     return res;
+// }
+
 //----------------------------------------------------------------------------------------------
 // Задача 60. ...Сформируйте трёхмерный массив из неповторяющихся двузначных чисел.
 // Напишите программу, которая будет построчно выводить массив,
@@ -155,7 +156,62 @@ int[,] MultiplyArray(int[,] array1, int[,] array2)
     // 34(1,0,0) 41(1,1,0)
     // 27(0,0,1) 90(0,1,1)
     // 26(1,0,1) 55(1,1,1)
-// Console.WriteLine("Задача 60:");
+Console.WriteLine("Задача 60:");
+//int[,,] arr60 = new int[,,]{{{66,27},{25,90}},{{34,26},{41,55}}};
+//PrintArray3d(arr60);
+PrintArray3d(GetArray3dDiffElements());
+int[,,] GetArray3dDiffElements(int m = 2, int n = 2, int o = 2, int minValue = 10, int maxValue = 99){
+    int[] arrayDiffEl= GetArrayDiffElements(m*n*o, minValue, maxValue);
+    int count = 0;
+    int[,,] res = new int[m, n, o];
+    for (int i = 0; i < m; i++)
+    {
+        for (int k = 0; k < n; k++)
+        {
+            for (int l = 0; l < o; l++)
+            {
+                res[i,k,l] = arrayDiffEl[count];
+                count++;
+            }
+        }
+    }
+    return res;
+}
+
+int[] GetArrayDiffElements(int length, int minValue, int maxValue){
+    int[] res = new int[length];
+    int r;
+    for (int i = 0; i < length; i++)
+    {  
+        r = new Random().Next(minValue, maxValue + 1);
+        for (int k = 0; k < i;)
+        {
+            if (r == res[k]) {
+                r = new Random().Next(minValue, maxValue + 1);
+                k = 0;
+            }
+            else k++;
+        }
+        res[i] = r;
+    }
+    return res;
+}
+
+void PrintArray3d(int[,,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int k = 0; k < array.GetLength(1); k++)
+        {
+            for (int l = 0; l < array.GetLength(2); l++)
+            {
+                Console.Write($"{array[k,l,i]}({k},{l},{i}) ");
+            }
+        Console.WriteLine();
+        }
+    }
+}
+
 
 //----------------------------------------------------------------------------------------------
 // Задача 62. Напишите программу, которая заполнит спирально массив 4 на 4.
